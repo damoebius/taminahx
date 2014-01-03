24cbf72c6c59b7f6476daddede871fef99cadd75
David Mouton <damo@tamina-online.com>
2014-01-03 12:48:26 +0100

    + ScriptLoader

--- tamina/src/Main.hx
+++ tamina/src/Main.hx
@@ -1,5 +1,6 @@
 package ;
-import org.tamina.utils.SWFObject;
+import org.tamina.net.URL;
+import org.tamina.net.ScriptLoader;
 import org.tamina.log.QuickLogger;
 class Main {
 
@@ -8,6 +9,7 @@ class Main {
 
     public static function main():Void{
         QuickLogger.info('test lib');
-        var swfo = new SWFObject();
+        var loader = new ScriptLoader();
+        loader.load( new URL('http://code.createjs.com/easeljs-0.7.1.min.js') );
     }
 }
--- tamina/src/org/tamina/net/ScriptLoader.hx
+++ tamina/src/org/tamina/net/ScriptLoader.hx
@@ -1,5 +1,39 @@
 package org.tamina.net;
+import js.html.Element;
+import org.tamina.log.QuickLogger;
+import js.html.ScriptElement;
+import js.html.HtmlElement;
+import js.Browser;
+import msignal.Signal.Signal0;
 class ScriptLoader {
+
+    public var completeSignal:Signal0;
+    public var errorSignal:Signal0;
+
+    private var _header:Element;
+
     public function new() {
+        completeSignal = new Signal0();
+        errorSignal = new Signal0();
+    }
+
+    public function load(url:URL):Void {
+        var script:ScriptElement = cast Browser.document.createElement('script');
+        QuickLogger.info('chargement de ' + url.path);
+        script.addEventListener('load', loadCompleteHandler) ;
+        script.addEventListener('error', loadErrorHandler) ;
+        script.src = url.path;
+        _header = cast Browser.document.getElementsByTagName('head')[0];
+        _header.appendChild(script);
+
+    }
+
+    private function loadCompleteHandler(event:Dynamic):Void {
+        QuickLogger.info('script loaded');
+        completeSignal.dispatch();
+    }
+
+    private function loadErrorHandler(event:Dynamic):Void {
+        errorSignal.dispatch();
     }
 }
