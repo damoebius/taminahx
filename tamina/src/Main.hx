7a1be916cb2414ffe413055daea74abbe2e9e8c5
David Mouton <damo@tamina-online.com>
2014-01-03 14:42:00 +0100

    + ScriptListLoader

--- tamina/src/Main.hx
+++ tamina/src/Main.hx
@@ -1,4 +1,5 @@
 package ;
+import org.tamina.net.ScriptListLoader;
 import org.tamina.net.URL;
 import org.tamina.net.ScriptLoader;
 import org.tamina.log.QuickLogger;
@@ -7,9 +8,13 @@ class Main {
     public function new() {
     }
 
-    public static function main():Void{
+    public static function main():Void {
         QuickLogger.info('test lib');
-        var loader = new ScriptLoader();
-        loader.load( new URL('http://code.createjs.com/easeljs-0.7.1.min.js') );
+        var loader = new ScriptListLoader();
+        var scripts = new Array<URL>();
+        scripts.push(new URL('http://code.createjs.com/easeljs-0.7.1.min.js'));
+        scripts.push(new URL('http://code.createjs.com/tweenjs-0.5.1.min.js'));
+        scripts.push(new URL('http://code.createjs.com/preloadjs-0.4.1.min.js'));
+        loader.load(scripts);
     }
 }
---
+++ tamina/src/org/tamina/net/ScriptListLoader.hx
@@ -0,0 +1,40 @@
+package org.tamina.net;
+import org.tamina.log.QuickLogger;
+import msignal.Signal;
+
+class ScriptListLoader {
+
+    public var completeSignal:Signal0;
+    public var errorSignal:Signal0;
+
+    private var _pool:Array<URL>;
+    private var _loader:ScriptLoader;
+
+    public function new() {
+        _loader = new ScriptLoader();
+        _loader.completeSignal.add(scriptCompleteHandler);
+        _loader.errorSignal.add(scriptErrorHandler);
+    }
+
+    public function load(scripts:Array<URL>):Void {
+        _pool = scripts;
+        loadNextScript();
+    }
+
+    private function scriptCompleteHandler():Void {
+        loadNextScript();
+    }
+
+    private function scriptErrorHandler():Void {
+        loadNextScript();
+    }
+
+    private function loadNextScript():Void {
+        if (_pool.length > 0) {
+            _loader.load(_pool.shift());
+        } else {
+            QuickLogger.info("ALL SCRIPTS LOADED");
+            completeSignal.dispatch();
+        }
+    }
+}
