183ace26b08e2e9f091c15c4844936fab7bd5dbb
David Mouton <damo@tamina-online.com>
2014-07-09 12:01:38 +0200

    + MouseEventType

---
+++ org/tamina/events/html/MouseEventType.hx
@@ -0,0 +1,8 @@
+package org.tamina.events.html;
+
+class MouseEventType {
+    static public inline var CLICK:String='click';
+    static public inline var MOUSE_MOVE:String='mousemove';
+    static public inline var MOUSE_DOWN:String='mousedown';
+    static public inline var MOUSE_UP:String='mouseup';
+}
