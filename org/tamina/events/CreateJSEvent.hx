b998e74f997e80ca5651bbc4e891a8d06647dfb4
damoebius <tintin33$>
2014-09-15 17:42:57 +0200

    invert color

--- org/tamina/events/CreateJSEvent.hx
+++ org/tamina/events/CreateJSEvent.hx
@@ -7,6 +7,8 @@ class CreateJSEvent {
     static public inline var MOUSE_UP:String='pressup';
     static public inline var MOUSE_MOVE:String='pressmove';
     static public inline var MOUSE_DOWN:String='mousedown';
+    static public inline var DOUBLE_CLICK:String='dblclick';
+    static public inline var CLICK:String='click';
     static public inline var TICKER_TICK:String='tick';
     static public inline var STAGE_MOUSE_UP:String='stagemouseup';
     static public inline var STAGE_MOUSE_MOVE:String='stagemousemove';
