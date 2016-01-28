90ff66dc07db4ba9ddb455fe002e155b297672d8
David Mouton <damo@tamina-online.com>
2016-01-28 17:53:17 +0100

    -BitmapUtils

--- org/tamina/utils/BitmapUtils.hx
+++ org/tamina/utils/BitmapUtils.hx
@@ -3,6 +3,6 @@ import org.tamina.html.MimeType;
 class BitmapUtils {
 
     public static function getMimeType(base64:String):MimeType{
-        return base64.substring(base64.indexOf(':') + 1, base64.indexOf(';');
+        return base64.substring(base64.indexOf(':') + 1, base64.indexOf(';'));
     }
 }
