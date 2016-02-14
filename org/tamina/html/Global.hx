62a49a6b14943df66b88380f7d5bccc91f2b3210
damoebius <tintin33$>
2016-02-14 11:05:28 +0100

    more docs

--- org/tamina/html/Global.hx
+++ org/tamina/html/Global.hx
@@ -1,11 +1,21 @@
 package org.tamina.html;
 import js.Browser;
+
+/**
+ * Global object give an accès to Window element
+ * @class Global
+ */
 class Global {
 
     private static var _instance:Global;
 
     private var _global:Dynamic;
 
+    /**
+    * @method getInstance
+    * @static
+    * @return {Global} a reference to Window Element
+    */
     public static function getInstance():Global{
        if(_instance == null){
             _instance = new Global();
@@ -17,7 +27,17 @@ class Global {
         _global = Browser.window;
     }
 
-    public function call(hostFunction:String, args : Array<Dynamic>):Void{
-        Reflect.callMethod(_global, Reflect.field(_global, hostFunction), args);
+    /**
+    * Call a dynamic function and return the result
+    * @method call
+    * @param hostFunction {String} the name of the function to call
+    * @param args Array<{Dynamic}>  arguments list
+    * @return {Dynamic} the call result
+    * @example
+     *      Global.getInstance().call('myFunction',[1,false]);
+     *
+    */
+    public function call(hostFunction:String, args : Array<Dynamic>):Dynamic{
+        return Reflect.callMethod(_global, Reflect.field(_global, hostFunction), args);
     }
 }
