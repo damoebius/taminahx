fb04302e2b9847e4ba30a51b37a5106cd9bd4623
damoebius <tintin33$>
2016-03-20 19:22:29 +0100

    more docs

--- org/tamina/i18n/Locale.hx
+++ org/tamina/i18n/Locale.hx
@@ -1,4 +1,10 @@
 package org.tamina.i18n;
+
+/**
+ * Locale Enum
+ * @class Locale
+ * @enum
+ */
 @:enum abstract Locale(String) from String to String {
     var ALBANIA = "sq_AL";
     var ALGERIA = "ar_DZ";
@@ -107,3 +113,641 @@ package org.tamina.i18n;
     var VIETNAM = "vi_VN";
     var YEMEN = "ar_YE";
 }
+
+/**
+ * @property ALBANIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ALGERIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ARGENTINA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property AUSTRALIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property AUSTRIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BAHRAIN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BELARUS
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BELGIUM
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BELGIUM_FR
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BOLIVIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BOSNIA_AND_HERZEGOVINA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BRAZIL
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property BULGARIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CANADA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CANADA_FR
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CHILE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CHINA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property COLOMBIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property COSTA_RICA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CROATIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CYPRUS
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property CZECH_REPUBLIC
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property DENMARK
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property DOMINICAN_REPUBLIC
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ECUADOR
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property EGYPT
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property EL_SALVADOR
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ESTONIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property FINLAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property FRANCE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property GERMANY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property GREECE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property GUATEMALA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property HONDURAS
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property HONG_KONG
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property HUNGARY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ICELAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property INDIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property INDIA_EN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property INDONESIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property IRAQ
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property IRELAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property IRELAND_EN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ISRAEL
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ITALY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property JAPAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property JORDAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property KUWAIT
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LATVIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LEBANON
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LIBYA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LITHUANIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LUXEMBOURG
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property LUXEMBOURG_DE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MACEDONIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MALAYSIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MALTA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MALTA_EN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MEXICO
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MONTENEGRO
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property MOROCCO
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property NETHERLANDS
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property NEW_ZEALAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property NICARAGUA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property NORWAY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property OMAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PANAMA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PARAGUAY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PERU
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PHILIPPINES
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property POLAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PORTUGAL
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property PUERTO_RICO
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property QATAR
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property ROMANIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property RUSSIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SAUDI_ARABIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SERBIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SERBIA_AND_MONTENEGRO
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SINGAPORE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SINGAPORE_EN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SLOVAKIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SLOVENIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SOUTH_AFRICA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SOUTH_KOREA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SPAIN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SPAIN_CATALAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SUDAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SWEDEN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SWITZERLAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SWITZERLAND_DE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SWITZERLAND_IT
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property SYRIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property TAIWAN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property THAILAND
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property TUNISIA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property TURKEY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property UKRAINE
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property UNITED_ARAB_EMIRATES
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property UNITED_KINGDOM
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property UNITED_STATES
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property UNITED_STATES_ES
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property URUGUAY
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property VENEZUELA
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property VIETNAM
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+/**
+ * @property YEMEN
+ * @type Locale
+ * @static
+ * @readOnly
+ */
+
--- org/tamina/i18n/Translation.hx
+++ org/tamina/i18n/Translation.hx
@@ -1,10 +1,42 @@
 package org.tamina.i18n;
+
+/**
+ * Transalation is an ITranslation's implementation<br>
+ * See {{#crossLink "LocalizationManager"}}{{/crossLink}}
+* @class Translation
+* @interface ITranslation
+* @module Tamina
+*/
 class Translation implements ITranslation {
 
+/**
+     * The translation's Id
+     * @property fieldName
+     * @type String
+     */
     public var fieldName:String;
+
+/**
+     * The locale
+     * @property locale
+     * @type Locale
+     */
     public var locale:Locale;
+
+/**
+     * The value
+     * @property value
+     * @type String
+     */
     public var value:String;
 
+/**
+    * @constructor
+    * @method new
+    * @param [fieldName] {String}
+    * @param [value] {String}
+    * @param [locale] {Locale}
+    */
     public function new(fieldName:String = "", value:String = "",locale:Locale = "") {
         this.locale = locale;
         this.fieldName = fieldName;
