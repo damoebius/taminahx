846eab4eb4fb07982a9b4b03fdcdbb214015ff97
Rudy Ges <rudy.geslin@gmail.com>
2016-03-24 17:04:17 +0100

    Added Drag & Focus event types

---
+++ org/tamina/events/html/DragEventType.hx
@@ -0,0 +1,81 @@
+package org.tamina.events.html;
+
+/**
+     * Drag API Events
+     * @class DragEventType
+     * @static
+     */
+class DragEventType {
+
+/**
+     * This event is fired when an element or text selection is being dragged.
+     * @property DRAG
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG:String='drag';
+
+/**
+     * This event is fired when a drag operation is being ended (by releasing a mouse button or hitting the escape key).
+     * @property DRAG_END
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_END:String='dragend';
+
+/**
+     * This event is fired when a dragged element or text selection enters a valid drop target.
+     * @property DRAG_ENTER
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_ENTER:String='dragenter';
+
+/**
+     * This event is fired when an element is no longer the drag operation's immediate selection target.
+     * @property DRAG_EXIT
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_EXIT:String='dragexit';
+
+/**
+     * This event is fired when a dragged element or text selection leaves a valid drop target.
+     * @property DRAG_LEAVE
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_LEAVE:String='dragleave';
+
+/**
+     * This event is fired when an element or text selection is being dragged over a valid drop target (every few hundred milliseconds).
+     * @property DRAG_OVER
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_OVER:String='dragover';
+
+/**
+     * This event is fired when the user starts dragging an element or text selection.
+     * @property DRAG_START
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DRAG_START:String='dragstart';
+
+/**
+     * This event is fired when an element or text selection is dropped on a valid drop target.
+     * @property DROP
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DROP:String='drop';
+}
---
+++ org/tamina/events/html/FocusEventType.hx
@@ -0,0 +1,27 @@
+package org.tamina.events.html;
+
+/**
+     * Focus API Events
+     * @class FocusEventType
+     * @static
+     */
+class FocusEventType {
+    
+/**
+     * The focus event is fired when an element has received focus. The main difference between this event and focusin is that only the latter bubbles.
+     * @property FOCUS
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var FOCUS:String='focus';
+    
+/**
+     * The blur event is fired when an element has lost focus. The main difference between this event and focusout is that only the latter bubbles.
+     * @property BLUR
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var BLUR:String='blur';
+}
