578e3f733fd1c93e0c567b4c77ffebcfba70befb
Rudy Ges <rudy.geslin@gmail.com>
2016-07-04 10:05:47 +0200

    1.0.27 - Added geom.Rectangle

---
+++ org/tamina/geom/Rectangle.hx
@@ -0,0 +1,18 @@
+package org.tamina.geom;
+
+class Rectangle {
+
+    public var left: Int;
+    public var top: Int;
+    public var right: Int;
+    public var bottom: Int;
+
+    public function new( left: Int, top: Int, right: Int, bottom: Int)
+    {
+        this.left = left; 
+        this.top = top;
+        this.right = right; 
+        this.bottom = bottom;
+    }
+    
+}
