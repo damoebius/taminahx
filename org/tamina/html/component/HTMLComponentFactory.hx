60aed6e030f9d8e2dcca83893cef45f4e7e8ad06
k <k@klabz.org>
2016-07-10 17:32:39 +0200

    Improved xtag usage

--- org/tamina/html/component/HTMLComponentFactory.hx
+++ org/tamina/html/component/HTMLComponentFactory.hx
@@ -33,15 +33,21 @@ class HTMLComponentFactory {
             pos : pos
         });
 
-        var tagExpr = className.toLowerCase().split('.').join('-');
-        if (cls.meta.has("tag")) {
-            var customTag:String = haxe.macro.ExprTools.getValue(cls.meta.extract("tag").pop().params[0]);
-            if (customTag.indexOf("-") > -1) {
-                tagExpr = customTag;
-            } else {
-                var tagPrefix = haxe.macro.Compiler.getDefine("tagPrefix");
-                if (tagPrefix != null) {
-                    tagExpr = tagPrefix + "-" + customTag;
+        var xtagExpr = className.toLowerCase().split('.').join('-');
+        if (cls.meta.has("view")) {
+            var viewParams = cls.meta.extract("view").pop().params;
+
+            if (viewParams.length > 1) {
+                var xtag:String = haxe.macro.ExprTools.getValue(viewParams[1]);
+                var xtagPrefix = haxe.macro.Compiler.getDefine("xtagPrefix");
+                if (xtagPrefix != null) {
+                    xtag = xtagPrefix + "-" + xtag;
+                }
+                
+                if (xtag.indexOf("-") > -1) {
+                    xtagExpr = xtag;
+                } else {
+                    Context.fatalError('Cannot register a custom component named "$xtag".\nCustom components names must contain at least one dash. You can prefix all your custom tags by compiling with -D xtagPrefix=myprefix', Context.currentPos());
                 }
             }
         }
@@ -51,7 +57,7 @@ class HTMLComponentFactory {
             pos: cls.pos,
             access: [AStatic],
             kind: FVar(macro : Bool, macro @:pos(cls.pos) {
-            org.tamina.html.component.HTMLApplication.componentsTagList.set($v{tagExpr}, $v{className});
+            org.tamina.html.component.HTMLApplication.componentsTagList.set($v{xtagExpr}, $v{className});
             true;
             })
         });
