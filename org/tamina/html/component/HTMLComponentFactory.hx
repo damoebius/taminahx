7f30f34847bd2b073360e3bfd99619eb57a07c73
Rudy Ges <rudy.geslin@gmail.com>
2016-08-16 15:13:18 +0200

    1.0.32 - Fixed HTMLComponents for Haxe 3.2.1

--- org/tamina/html/component/HTMLComponentFactory.hx
+++ org/tamina/html/component/HTMLComponentFactory.hx
@@ -100,7 +100,7 @@ class HTMLComponentFactory {
             access: [AStatic],
             kind: FVar(macro : Bool, macro @:pos(cls.pos) {
                 org.tamina.html.component.HTMLApplication.componentsXTagList.set($v{xtagExpr}.toLowerCase(), $v{className});
-                return true;
+                true;
             })
         });
 
