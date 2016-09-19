6ad9b5cb0e10132b3e36901127238936d860bdf7
Rudy Ges <rudy.geslin@gmail.com>
2016-09-19 16:22:38 +0200

    1.0.47 Added filterById to ObjectUtils

--- org/tamina/utils/ObjectUtils.hx
+++ org/tamina/utils/ObjectUtils.hx
@@ -11,4 +11,18 @@ class ObjectUtils {
             }
         }
     }
+
+    /**
+     * Retourne une fonction "filter" (classiquement, pour un usage en Array.filter())
+     * Triant les objets du tableau pour retourner celui (ou ceux) qui a le même id
+     * Que l'objet passé initialement
+     *
+     * @method filterById[T]
+     * @param   source {T} L'objet à rechercher
+     */
+    public static function filterById<T:{id:Dynamic}>(source:T):T->Bool {
+        return function(obj:T):Bool {
+            return source.id == obj.id;
+        };
+    }
 }
