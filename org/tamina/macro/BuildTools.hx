6b306b9cdbaa982951835acc27ad77950087904b
Rudy Ges <rudy.geslin@gmail.com>
2017-01-06 16:20:11 +0100

    Macros: haxe 3.2.1 compatible

--- org/tamina/macro/BuildTools.hx
+++ org/tamina/macro/BuildTools.hx
@@ -1,5 +1,11 @@
 package org.tamina.macro;
 
+import haxe.macro.Context;
+import haxe.macro.Expr;
+import haxe.macro.ExprTools;
+import haxe.macro.Type;
+import haxe.macro.TypeTools;
+
 import org.tamina.macro.MacroTools.match;
 
 class BuildTools {
--- org/tamina/macro/import.hx
+++
@@ -1,5 +0,0 @@
-import haxe.macro.Context;
-import haxe.macro.Expr;
-import haxe.macro.ExprTools;
-import haxe.macro.Type;
-import haxe.macro.TypeTools;
