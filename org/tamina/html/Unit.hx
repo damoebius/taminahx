8c143fd983967663a761640c8f13e2482ae0dd16
dmouton <d.mouton@happy-technologies.com>
2017-08-30 10:36:48 +0200

    1.2.3 : New Units

--- org/tamina/html/Unit.hx
+++ org/tamina/html/Unit.hx
@@ -2,4 +2,10 @@ package org.tamina.html;
 
 class Unit {
     public static inline var PIXEL:String = "px";
+    public static inline var RADIAN:String = "rad";
+    public static inline var EM:String = "em";
+    public static inline var PERCENT:String = "%";
+    public static inline var POINT:String = "pt";
+    public static inline var REM:String = "rem";
+    public static inline var DEGREE:String = "deg";
 }
